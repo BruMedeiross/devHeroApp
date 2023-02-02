31bc841fc5841df3dccd69e1b4138df85f8fc794
Tiago Aguiar <tiago-aguiar@users.noreply.github.com>
2023-02-02 11:40:35 -0300

    ViewModel com Mediator

--- app/src/main/java/com/brunadev/devheroapp/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/brunadev/devheroapp/login/viewmodel/LoginViewModel.kt
@@ -13,8 +13,45 @@ class LoginViewModel(private val repository: DevHeroRepository = DevHeroImpl()) 
 
     var email = ObservableField("")
     var password = ObservableField("")
-
+    
     private val liveData = MutableLiveData<UserResponse?>()
+    
+    /* 
+    // TODO: Adicionar o liveData para 2 cases: Formulario e Server,
+    // Sendo que, a resposta do server, deverá ser intermediada pelo MediatorLiveData, no qual
+    // irá capturar a respostas do LiveData presente no Repository
+    
+    private val _userState = MediatorLiveData<UserResponse?>()
+    val userState: LiveData<UserResponse?> get() = _userState
+
+    private val _formState = MutableLiveData<Boolean>()
+    val formState: LiveData<Boolean> get() = _formState
+
+    // evento -> input (não há retorno na funcao, visto que, o dado que será alterado
+    // é o _formState
+    fun doLogin() {
+       // 1 validar
+        if (validate()) {
+            _formState.postValue(true)
+        } else {
+            _formState.postValue(false)
+        }
+    }
+
+    fun request() {
+        val liveDataRepo = repository.loginUser(
+            User(email = email.get()?.trim(), password = password.get()?.trim())
+        )
+        // Aqui o liveData do repository é adicionado como fonte de dados (source)
+        // para o MediatorLiveData. Uma vez que o dado é alterado (postValue no repository)
+        // o UserResponse é capturado dentro do bloco do Mediator.
+        // Dessa forma, ao expor o Mediator para a View, ela poderá observar a mudança de estado
+        // que ocorreu no Repository, graças ao Mediator.
+        _userState.addSource(liveDataRepo) { userResponse ->
+           _userState.postValue(userResponse)
+        }
+    }
+    */
 
     fun doLogin(): LiveData<UserResponse?> {
         val data = if (validate()) {
